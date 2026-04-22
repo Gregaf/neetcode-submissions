a2a4bce13331a59e7c6bad294eede3e46e14a9bd
Gregory Freitas <gregfreitas1997@gmail.com>
2026-04-21 23:01:05 -0400

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if len(intervals) == 0:
+            return True
+
+        intervals = sorted(intervals, key=lambda x:x.start)
+
+        for i in range(len(intervals) - 1):
+            interval_curr = intervals[i]
+            interval_next = intervals[i + 1]
+
+            if interval_curr.end > interval_next.start:
+                return False
+        
+        return True
+                    
+            
